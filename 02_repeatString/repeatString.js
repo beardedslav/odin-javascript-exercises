0133378e81cfef5de9aa5d76acf67408789e14eb
Krzysztof Malinowski <krzysztof@malinowski.scot>
2023-07-09 16:51:57 +0100

    Exercise 2 solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (what, howMany) {
+  if (howMany < 0) return "ERROR";
+  let res = "";
+  for (let index = 0; index < howMany; index++) {
+    res += what;
+  }
+  return res;
 };
 
 // Do not edit below this line
