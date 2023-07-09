1502a5fae9899ab24a26f175f173446f7af9179f
Krzysztof Malinowski <krzysztof@malinowski.scot>
2023-07-09 17:04:33 +0100

    Exercise 4 solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (inputArray, ...itemsToRemove) {
+  res = inputArray;
+  for (let index = 0; index < itemsToRemove.length; index++) {
+    res = res.filter((el) => el !== itemsToRemove[index]);
+  }
+  return res;
 };
 
 // Do not edit below this line
