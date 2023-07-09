d69fa21b937b4e2602ba80561384e64ed9afa52d
Krzysztof Malinowski <krzysztof@malinowski.scot>
2023-07-09 17:12:00 +0100

    Exercise 5 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function (lowerBound, upperBound) {
+  if (lowerBound > upperBound) {
+    let temp = upperBound;
+    upperBound = lowerBound;
+    lowerBound = temp;
+  }
+  if (
+    typeof lowerBound !== "number" ||
+    typeof upperBound !== "number" ||
+    lowerBound < 0 ||
+    upperBound < 0
+  ) {
+    return "ERROR";
+  }
+  allNums = Array.from(
+    new Array(upperBound - lowerBound + 1),
+    (x, i) => i + lowerBound
+  );
+  let sum = 0;
+  allNums.forEach((element) => {
+    sum += element;
+  });
+  return sum;
 };
 
 // Do not edit below this line
