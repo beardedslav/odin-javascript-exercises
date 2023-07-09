bdba6e20f48183bd810b0153bc7cd69a2abdc8c0
Krzysztof Malinowski <krzysztof@malinowski.scot>
2023-07-09 16:57:11 +0100

    Exercise 3 solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  res = "";
+  for (let index = input.length - 1; index >= 0; index--) {
+    res += input[index];
+  }
+  return res;
 };
 
 // Do not edit below this line
